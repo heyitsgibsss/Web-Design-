6a5cffdbc0b531e9515ea61b6cb9eb34e901a211
heyitsgibsss <gilbertorlands28@gmail.com>
2023-10-31 16:53:45 +0800

    Web Design

---
+++ Web Design/rumah/rumah.js
@@ -0,0 +1,7 @@
+document.getElementById("bukaPintu").addEventListener("click", function() {
+    document.getElementById("pintu").style.transform = "rotateY(-90deg)";
+});
+
+document.getElementById("tutupPintu").addEventListener("click", function() {
+    document.getElementById("pintu").style.transform = "rotateY(0deg)";
+});
